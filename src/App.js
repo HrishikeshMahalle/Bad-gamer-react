b7b5756e03f3588563029848671cc4a742e6bfde
Hrishikesh Mahalle <rushimahalle09@gmail.com>
2022-04-06 23:14:37 +0530

    normal code update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 
 import Home from './Components/Home/Home';
-import Login from './Components/Login/login';
-import Cart from './Components/Cart/cart';
-import Wishlist from './Components/Wishlist/wishlist';
-import Merch from './Components/Product-Page/merch';
-import Games from './Components/Product-Page/games';
-import Hardware from './Components/Product-Page/hardware';
+
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 import { makeServer } from './server';
 
@@ -18,13 +13,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route exact path="" element={<Home/>}/>
-            <Route path="Login" element={<Login/>}/>
-            <Route path="Cart" element={<Cart />}/>
-            <Route path="Wishlist" element={<Wishlist/>}/>
-            <Route path="Merch" element={<Merch/>}/>
-            <Route path="Games" element={<Games/>}/>
-            <Route path="Products" element={<Hardware/>}/>
+            <Route exact path="/" element={<Home/>}/>
         </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {GlobalProvider} from './Context/GlobalState'
-import {DataProvider} from './Context/dataContext'
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-  <DataProvider>
-      <GlobalProvider>
         <App />
-      </GlobalProvider>
-  </DataProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
